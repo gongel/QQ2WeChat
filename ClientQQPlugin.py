a403deea6618b48570ab459a90609e88b05b8b82
gongel <1324522527@qq.com>
2018-09-12 12:35:56 +0800

    修复： 不用转发自己发的消息; 转发格式调整.

--- ClientQQPlugin.py
+++ ClientQQPlugin.py
@@ -4,6 +4,6 @@ def onQQMessage(bot,contact,member,content):
     s=socket.socket()
     port=6969
     s.connect(('127.0.0.1',port))
-    if contact.ctype=='buddy':
-        s.send(b""+(contact.name+':'+content))
-        bot.SendTo(contact,'Hello {},QQ暂时不上线，请加我微信XXXXXXXXXX，谢谢。'.format(contact.name))
+    if contact.ctype=='buddy' and bot.isMe(contact,member)==False:
+        s.send(b"QQ message:\n"+(contact.name+'：'+content))
+        bot.SendTo(contact,'Hello {}，你的信息我已经收到。请加我微信1324522527，谢谢。'.format(contact.name))
